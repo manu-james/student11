1b9daf03c2961a839e405dcbf457671311553a85
manu-james <mca524@rajagiri.edu>
2021-03-17 12:47:53 +0530

    levage

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -3,18 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=ta, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
     <title>Document</title>
 </head>
-<div><h1>Student</h1></div>
 <body>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <a class="navbar-brand" href="#">Student</a>
+    <a class="navbar-brand" href="#">Navbar</a>
     <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
@@ -31,26 +30,22 @@
     </div>
   </div>
 </nav>
-<div class="container"><table class="table table-borderless">
+<div><h1>Contacts</h1></div>
+<body><div class="container"><table class="table table-borderless">
     
     <tr>
-        <td>Name</td>
+        <td>name</td>
         <td><input type="text" class="form-control"></td>
     </tr>
     <tr>
-        <td>Rollno</td>
-        <td><input type="text" class="form-control"></td>
+        <td>message</td>
+        <td><input type="textarea" class="form-control"></td>
     </tr>
     <tr>
-        <td>College</td>
+        <td>phno</td>
         <td><input type="text" class="form-control"></td>
     </tr>
-    <tr>
-        <td>admission no</td>
-        <td><input type="text" class="form-control"></td>
-    </tr>
-    <tr>
-        <td></td>
+   
         <td><Button class="btn-btn-sucess">Submit</Button></td>
     </tr>   
      </table></div>
--- resources/views/faculty.blade.php
+++ resources/views/faculty.blade.php
@@ -3,11 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=ta, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
     <title>Document</title>
 </head>
-<div><h1>Student</h1></div>
 <body>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
@@ -31,22 +30,31 @@
     </div>
   </div>
 </nav>
-<div class="container"><table class="table table-borderless">
+<div><h1>Faculty</h1></div>
+<body><div class="container"><table class="table table-borderless">
     
     <tr>
-        <td>Name</td>
+        <td>Department</td>
         <td><input type="text" class="form-control"></td>
     </tr>
     <tr>
-        <td>Rollno</td>
+        <td>address</td>
         <td><input type="text" class="form-control"></td>
     </tr>
     <tr>
-        <td>College</td>
+        <td>phno</td>
         <td><input type="text" class="form-control"></td>
     </tr>
     <tr>
-        <td>admission no</td>
+        <td>education</td>
+        <td><input type="text" class="form-control"></td>
+    </tr>
+    <tr>
+        <td>qualification</td>
+        <td><input type="text" class="form-control"></td>
+    </tr>
+    <tr>
+        <td>collge</td>
         <td><input type="text" class="form-control"></td>
     </tr>
     <tr>
@@ -54,6 +62,5 @@
         <td><Button class="btn-btn-sucess">Submit</Button></td>
     </tr>   
      </table></div>
-    
 </body>
 </html>
