9486181d855287ce93d47bcbb4aedcd64ef2161b
manu-james <mca524@rajagiri.edu>
2021-03-17 11:56:12 +0530

    levage

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=ta, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<div><h1>Student</h1></div>
+<body><div class="container"><table class="table table-borderless">
+    
+    <tr>
+        <td>Name</td>
+        <td><input type="text" class="form-control"></td>
+    </tr>
+    <tr>
+        <td>Rollno</td>
+        <td><input type="text" class="form-control"></td>
+    </tr>
+    <tr>
+        <td>College</td>
+        <td><input type="text" class="form-control"></td>
+    </tr>
+    <tr>
+        <td>admission no</td>
+        <td><input type="text" class="form-control"></td>
+    </tr>
+    <tr>
+        <td></td>
+        <td><Button class="btn-btn-sucess">Submit</Button></td>
+    </tr>   
+     </table></div>
+    
+</body>
+</html>
